6810db2b8d859403f6645473924443ac6f1ddcb2
Herve Desrosiers <herve_desrosiers@hotmail.com>
2022-07-12 08:38:49 -0400

    Add a mouse over listener on divs to persist fill

--- javascript.js
+++ javascript.js
@@ -4,7 +4,9 @@ const grid = document.querySelector('.grid');
 for (let r = 0; r < GRID_SIZE; r++) {
   for (let c = 0; c < GRID_SIZE; c++) {
     const child = document.createElement('div');
-    child.classList.add('px', r, c);
+    child.addEventListener('mouseover', (e) => {
+      e.target.classList.add('fill');
+    });
     grid.appendChild(child);
   }
 }
