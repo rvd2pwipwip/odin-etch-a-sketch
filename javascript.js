0eef2979a7937458a3bb7db7b637c268572b9c15
Herve Desrosiers <herve_desrosiers@hotmail.com>
2022-07-12 09:12:01 -0400

    Build a fixed size square grid with editable number of rows/column

--- javascript.js
+++ javascript.js
@@ -1,8 +1,18 @@
-const GRID_SIZE = 16;
+const GRID_SIZE = 800;
 const grid = document.querySelector('.grid');
-// grid.textContent = 'Hello, Grid!';
-for (let r = 0; r < GRID_SIZE; r++) {
-  for (let c = 0; c < GRID_SIZE; c++) {
+let gridCells = 16;
+
+const customGrid = (gridCells) => {
+  grid.style.height = `${GRID_SIZE}px`;
+  grid.style.width = `${GRID_SIZE}px`;
+  grid.style.gridTemplateRows = `repeat(${gridCells}, 1fr)`;
+  grid.style.gridTemplateColumns = `repeat(${gridCells}, 1fr)`;
+};
+
+customGrid(gridCells);
+
+for (let r = 0; r < gridCells; r++) {
+  for (let c = 0; c < gridCells; c++) {
     const child = document.createElement('div');
     child.addEventListener('mouseover', (e) => {
       e.target.classList.add('fill');
